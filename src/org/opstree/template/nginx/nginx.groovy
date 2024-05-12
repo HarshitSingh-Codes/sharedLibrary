29556a3e698d166415c484f335420dba7db59862
Harshit Singh <harshitpanu7900@gmail.com>
2024-05-12 22:10:10 +0530

    Update nginx.groovy

--- src/org/opstree/template/nginx/nginx.groovy
+++ src/org/opstree/template/nginx/nginx.groovy
@@ -4,10 +4,11 @@ import org.opstree.common.*
 import org.opstree.launchTemplate.*
 import org.opstree.packer.*
 
-def call(String url, String creds, String branch, boolean runPacker, String packerFileName){
+def call(String url, String creds, String branch, boolean runPacker, String packerFileName, boolean runTerraform, String terraformDirPath){
     
     def gitCheckout = new gitCheckout()
     def packer = new runPacker()
+    def terraform = new templateUtils()
     gitCheckout.call(url, creds, branch)
 
     if (runPacker) {
@@ -21,6 +22,20 @@ def call(String url, String creds, String branch, boolean runPacker, String pack
     else {
         script {
             print("skipping run packer")
+        }    
+    }
+
+    if (runTerraform) {
+        terraform.terraformInit(String terraformDirPath)
+        terraform.terraformPlan(String terraformDirPath)
+    } 
+    // else if (action == 'destroy') {
+        //     approval.call(destroyMsg)
+        //     utils.destroy(rootPath, childPath)
+    // } 
+    else {
+        script {
+            print("skipping run terraform")
         }    
     }
 }
