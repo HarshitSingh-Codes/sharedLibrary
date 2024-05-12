946db429de13112d78776467d7da39f441d3ea3e
Harshit Singh-Codes <harshitpanu7900@gmail.com>
2024-05-12 18:15:16 +0530

    updated packer Utils

--- src/org/opstree/packer/runPacker.groovy
+++ src/org/opstree/packer/runPacker.groovy
@@ -1,1 +1,16 @@
+package org.opstree.common
 
+def packerInit() {
+    stage('packer init') {
+        script {
+            sh "packer init"
+        }
+    }
+}
+def packerBuild(String fileName) {
+    stage('packer build') {
+        script {
+            sh "packer build ${fileName}"
+        }
+    }
+}
