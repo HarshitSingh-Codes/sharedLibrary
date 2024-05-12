8ea3a5ee3a5bd30854349d009220e9a96db06b95
Harshit Singh-Codes <harshitpanu7900@gmail.com>
2024-05-12 18:25:54 +0530

    updated template

--- src/org/opstree/template/nginx/nginx.groovy
+++ src/org/opstree/template/nginx/nginx.groovy
@@ -4,10 +4,21 @@ import org.opstree.common.*
 import org.opstree.launchTemplate.*
 import org.opstree.packer.*
 
-def call(String url, String creds, String branch){
+def call(String url, String creds, String branch, boolean runPacker, String packerFileName){
     
     def gitCheckout = new gitCheckout()
-
+    def packer = new runPacker()
     gitCheckout.call(url, creds, branch)
-    
+
+    if (runPacker) {
+            packer.packerInit()
+            packer.packerBuild(packerFileName)
+    } 
+    // else if (action == 'destroy') {
+        //     approval.call(destroyMsg)
+        //     utils.destroy(rootPath, childPath)
+    // } 
+    else {
+            error("Skipping run Packer")
+    }
 }
