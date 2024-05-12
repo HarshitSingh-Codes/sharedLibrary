501a9d1ccd88e41e3d5f8851a2a061a1ba6adaf8
Harshit Singh <harshitpanu7900@gmail.com>
2024-05-12 16:29:00 +0530

    Create templateUtils.groovy

---
+++ vars/org/opstree/launchTemplate/templateUtils.groovy
@@ -0,0 +1,9 @@
+package org.opstree.launchTemplate
+
+def call(String url, String creds, String branch) {
+    stage('Clone') {
+        script {
+            git branch: "${branch}", credentialsId: "${creds}", url: "${url}"
+        }
+    }
+}
