5d5a996cc2ce2d78da74430c842221687479559d
Harshit Singh-Codes <harshitpanu7900@gmail.com>
2024-05-12 16:54:59 +0530

    updated template

--- src/org/opstree/template/nginx/nginx.groovy
+++ src/org/opstree/template/nginx/nginx.groovy
@@ -4,7 +4,7 @@ import org.opstree.common.*
 import org.opstree.launchTemplate.*
 import org.opstree.packer.*
 
-def call(String url, String creds, String branch, String zapVersion, String currentWorkspace){
+def call(String url, String creds, String branch){
     
     def gitCheckout = new gitCheckout()
 
