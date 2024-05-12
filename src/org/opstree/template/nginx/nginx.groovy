1ea4baf8e4620c0f3968ce9e0d70df647e80f2be
Harshit Singh <harshitpanu7900@gmail.com>
2024-05-12 22:21:17 +0530

    Update nginx.groovy

--- src/org/opstree/template/nginx/nginx.groovy
+++ src/org/opstree/template/nginx/nginx.groovy
@@ -9,16 +9,25 @@ def call(String url, String creds, String branch, boolean runPacker, String pack
     def gitCheckout = new gitCheckout()
     def packer = new runPacker()
     def terraform = new templateUtils()
+    def approval = new approval()
+
+    def applyMsg = 'Do you want to apply the changes ?'
+    def destroyMsg = 'Do you want to destroy the changes ?'
+    
     gitCheckout.call(url, creds, branch)
 
     if (runPacker) {
-            packer.packerInit(packerFileName)
-            packer.packerBuild(packerFileName)
+        packer.packerInit(packerFileName)
+        packer.packerBuild(packerFileName)
+        if (action == 'apply') {
+            approval.call(applyMsg)
+            // utils.apply(rootPath, childPath)
+        } 
+        else if (action == 'destroy') {
+            approval.call(destroyMsg)
+            // utils.destroy(rootPath, childPath)
+        }
     } 
-    // else if (action == 'destroy') {
-        //     approval.call(destroyMsg)
-        //     utils.destroy(rootPath, childPath)
-    // } 
     else {
         script {
             print("skipping run packer")
