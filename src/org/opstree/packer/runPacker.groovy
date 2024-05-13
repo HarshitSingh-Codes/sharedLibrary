dc7c0d3f6ef5ad9eaba4e1dbc288defe4132bff1
Harshit Singh <harshitpanu7900@gmail.com>
2024-05-13 15:25:08 +0530

    Update runPacker.groovy

--- src/org/opstree/packer/runPacker.groovy
+++ src/org/opstree/packer/runPacker.groovy
@@ -11,11 +11,12 @@ def packerBuild(String fileName, String amiVersion) {
     stage('packer build') {
         script {
             //sh "packer build ${fileName}"
-            sh """packer build -force \
-              -var 'golden_ami_name=golden-ami' \
-              -var 'image_name=nginx' \
-              -var 'image_version=${amiVersion}' \
-              -var 'filePath=./index2.html' ${fileName}
+            sh """
+            packer build -force \
+            -var 'golden_ami_name=golden-ami' \
+            -var 'image_name=nginx' \
+            -var 'image_version=${amiVersion}' \
+            -var 'filePath=./index2.html' ${fileName}
             """
             sh"""
             export AMI_NAME=$(jq -r '.builds[] | select(.name == "nginx-ami") | .custom_data.ami_name' manifest.json)
