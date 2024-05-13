e5acc8b637d057c14918c583bae77e026c025325
Harshit Singh <harshitpanu7900@gmail.com>
2024-05-13 17:25:15 +0530

    Create awsCliUtils.groovy

---
+++ src/org/opstree/launchTemplate/awsCliUtils.groovy
@@ -0,0 +1,14 @@
+package org.opstree.launchTemplate
+
+def updateLT(String amiID) {
+    stage('Update launch template') {
+        script {
+            sh '''
+            aws ec2 create-launch-template-version \
+            --launch-template-id lt-0e0cf0cd693670427 \
+            --version-description WebVersion  \
+            --launch-template-data '{"ImageId":${amiID}}' 
+            '''
+        }
+    }
+}
