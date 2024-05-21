e28ce806f9b7744ef1df42cdc8dde8c55663a91f
Harshit Singh <harshitpanu7900@gmail.com>
2024-05-21 11:52:17 +0530

    update  deployeInfra

--- src/org/opstree/template/deployInfra/deployInfra.groovy
+++ src/org/opstree/template/deployInfra/deployInfra.groovy
@@ -4,38 +4,45 @@ import org.opstree.common.*
 import org.opstree.launchTemplate.*
 import org.opstree.packer.*
 
-def checkout(String url, String creds, String branch){
+def call(Map step_params){
     
     def gitCheckout = new gitCheckout()
-    
-    gitCheckout.call(url, creds, branch)
-}
-
-def runPacker(String packerFileName, String amiVersion){
-    
     def packer = new runPacker()
-    
-    packer.packerInit(packerFileName)
-    packer.packerBuild(packerFileName, amiVersion)
-}
-
-def updateLaunchTemplate(String templateID, String sourceVersion, versionDescription){
-    
-    def update =  new awsCliUtils()
-    
-    def amiID = sh(
-        script: '''
-        jq -r '.builds[].artifact_id' manifest.json | cut -d ":" -f2
-        ''',
-        returnStdout: true
-        ).trim()    
-    
-    update.updateLaunchTemplate(templateID, amiID, sourceVersion, versionDescription)
-}
-
-def instanceRefresh(String asgConfig) {
-    
+    def updateLaunchTemplate =  new awsCliUtils()
     def instanceRefresh =  new awsCliUtils()
 
-    instanceRefresh.startInstanceRefresh(asgConfig)
+    if (step_params.gitCheckout == true) {
+        url = "${step_params.url}"
+        creds = "${step_params.creds}"
+        branch = "${step_params.branch}"
+        gitCheckout.call(url, creds, branch)
+    }
 }
+// def runPacker(String packerFileName, String amiVersion){
+    
+//     def packer = new runPacker()
+    
+//     packer.packerInit(packerFileName)
+//     packer.packerBuild(packerFileName, amiVersion)
+// }
+
+// def updateLaunchTemplate(String templateID, String sourceVersion, versionDescription){
+    
+//     def update =  new awsCliUtils()
+    
+//     def amiID = sh(
+//         script: '''
+//         jq -r '.builds[].artifact_id' manifest.json | cut -d ":" -f2
+//         ''',
+//         returnStdout: true
+//         ).trim()    
+    
+//     update.updateLaunchTemplate(templateID, amiID, sourceVersion, versionDescription)
+// }
+
+// def instanceRefresh(String asgConfig) {
+    
+//     def instanceRefresh =  new awsCliUtils()
+
+//     instanceRefresh.startInstanceRefresh(asgConfig)
+// }
